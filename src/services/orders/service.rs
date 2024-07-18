8d3379149aa27d58a93640628223cd0f3e6b15f3
RhoNit <ranitbiswas.cs@gmail.com>
2024-07-18 18:41:15 +0000

    added get_order_with_all_payments() functionality

--- src/services/orders/service.rs
+++ src/services/orders/service.rs
@@ -3,10 +3,10 @@ use tonic::{Request, Response, Status};
 use crate::models;
 use crate::orders::v1::{
     order_service_server::OrderService, Empty, GetOrderRequest, GetOrderResponse,
-    GetPaymentRequest, GetPaymentResponse, ListOrderResponse,
+    GetPaymentRequest, GetPaymentResponse, ListOrderResponse, GetOrderWithAllPaymentsResponse
 };
 
-use super::utils::get_payment_response;
+use super::utils::{ get_payment_response, get_all_payments_response_for_an_order };
 
 #[derive(Default)]
 pub struct OrderServer {}
@@ -52,5 +52,23 @@ impl OrderService for OrderServer {
         let order = models::Order::find(payment.order_id)?;
 
         Ok(Response::new(get_payment_response(payment, order)))
+    }
+
+    // handler function to get an order along with all its payments
+    async fn get_order_with_all_payments(
+        &self,
+        request: Request<GetOrderRequest>
+    ) -> Result<Response<GetOrderWithAllPaymentsResponse>, Status> {
+        println!("Got a request: {:#?}", &request);
+
+        let order_id = request.into_inner().order_id;
+        let order = models::Order::find(order_id)?;
+
+        let payments_resp = get_all_payments_response_for_an_order(order_id);
+        
+        Ok(Response::new(GetOrderWithAllPaymentsResponse {
+            order: Some(GetOrderResponse::from(order)),
+            payments_list: payments_resp
+        }))
     }
 }
