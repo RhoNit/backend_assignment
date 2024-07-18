2409b431d07d1e95aef6c066ac57294b540d31eb
RhoNit <ranitbiswas.cs@gmail.com>
2024-07-18 18:40:10 +0000

    implemented a utility function to construct a payment response type of list of GetOnlyPaymentDetails

--- src/services/orders/utils.rs
+++ src/services/orders/utils.rs
@@ -1,6 +1,10 @@
 use crate::models;
 
-use crate::orders::v1::{GetOrderResponse, GetPaymentResponse};
+use crate::orders::v1::{
+    GetOrderResponse, 
+    GetPaymentResponse, 
+    GetOnlyPaymentDetails
+};
 
 impl From<models::Order> for GetOrderResponse {
     fn from(order: models::Order) -> GetOrderResponse {
@@ -21,4 +25,23 @@ pub fn get_payment_response(payment: models::Payment, order: models::Order) -> G
         status: payment.status,
         order: Some(GetOrderResponse::from(order)),
     }
+}
+
+// utility function to construct a reponse type of GetOnlyPaymentDetails
+pub fn get_all_payments_response_for_an_order(order_id: i32) -> Vec<GetOnlyPaymentDetails> {
+    // fetch all payments for a particular order (by order_id)
+    let payments = models::Payment::find_by_order_id(order_id);
+
+    // construct reponse as per GetOnlyPaymentDetails structure
+    let payment_details_reponse = 
+        payments.unwrap()
+            .into_iter()
+            .map(|payment| GetOnlyPaymentDetails {
+                payment_id: payment.id,
+                created_at: payment.created_at,
+                status: payment.status
+            })
+            .collect();
+    
+    payment_details_reponse
 }
